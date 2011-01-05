68d2e7b4ad18310c015238bf9f72f75bc347a066
mathias.hodler <mathias.hodler@c2443d48-5877-2981-9430-afcde90d5691>
2011-01-05 15:57:35 +0000

    -ImageManager angefangen zum Verwalten der Bilder -Bild wird Zoom und Scrollbar angezeigt

--- main.m
+++ main.m
@@ -1,17 +1,17 @@
 //
 //  main.m
-//  igstd
+//  picture-test
 //
-//  Created by apfel on 1/3/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Created by Markus Holoch on 29.12.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 int main(int argc, char *argv[]) {
+    
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     int retVal = UIApplicationMain(argc, argv, nil, nil);
     [pool release];
     return retVal;
 }
-
